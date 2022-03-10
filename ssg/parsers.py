8861790cc0a65e3352da975c360e42401a3fb767
Patrick Davies <patrick.davies@daugherty.com>
2022-03-10 16:05:11 -0600

    Update parsers.py

---
+++ ssg/parsers.py
@@ -0,0 +1,11 @@
+from typing import ClassVar, List
+from pathlib import Path
+
+class Parser:
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
