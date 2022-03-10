13544f472c9b512f1cee829a8fa21f3250b8b8e5
Patrick Davies <patrick.davies@daugherty.com>
2022-03-10 15:42:01 -0600

    Update site.py

--- ssg/site.py
+++ ssg/site.py
@@ -7,7 +7,7 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest + "/" + path.relative_to()
+        directory = self.dest / path.relative_to(self.source)
         mkdir (directory, parents=True, exist_ok=True)
 
     def build(self):
