fedc7adf69e60e7cc62b3613d4eca8196266f701
Patrick Davies <patrick.davies@daugherty.com>
2022-03-11 11:36:08 -0600

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,7 @@
 from os import mkdir
 from pathlib import Path
+import sys
+
 class Site:
 
     def __init__(self, source, dest, parsers = None):
@@ -31,4 +33,8 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print("Not Implemented")
+            self.error("No parser for the {} extension, file skipped!".format(path.suffix))
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
